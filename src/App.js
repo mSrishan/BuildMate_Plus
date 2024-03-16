30b4fcc9a6b0fa116863e1220f1cc6ee2f6faa3d
Pasindu Pathum <gappathum@std.appsc.sab.ac.lk>
2024-03-16 18:42:06 +0530

    Hi

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Navigate } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div >
+    <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Navbar.css'
+
+import logo from '../Assets/logo2.png'
+const Navbar = () => {
+  return (
+    <section className='home1'>
+      <div className='navbar'>
+        <div className='nav-logo'>
+          <img src={logo} alt=''/>
+        </div>
+
+        <ul className='nav-menu'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Blogs</li>
+          <li>Contact</li>
+          <li>English</li>
+        </ul>
+
+        <div className='btn'>
+          <button className='signbtn'>Sign in</button>
+          <button className='joinbtn'>Join</button> 
+        </div>
+    </div>
+    </section>
+    
+  )
+}
+
+export default Navbar
