342e32bd7fe4b9d772abc2abfec29e542855adca
KCDasanayaka <116500659+KCDasanayaka@users.noreply.github.com>
2024-05-11 22:43:31 +0530

    Responsive Homepage and Loginpage

---
+++ Build_Mate_fe/src/Components/Footer/footer.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './footer.css';
+import logoImage from '../Assets/logo2.png';
+import gps from '../Assets/gps.png';
+import email from '../Assets/email(1).png';
+import facebook from '../Assets/facebook.png';
+import phone from '../Assets/phone-call(1).png';
+import twitter from '../Assets/twitter.png';
+import instagram from '../Assets/instagram.png';
+import linkedin from '../Assets/linkedin.png';
+
+const footer= () => {
+    const openFacebookPage = () => {
+        window.open("https://www.facebook.com", "_blank");
+      };
+    const openInsta = () => {
+        window.open("https://www.instagram.com", "_blank")
+    }
+    const openLinkedin = () => {
+        window.open("https://www.linkedin.com", "_blank")
+    }  
+    const openTwitter = () =>{
+        window.open("https://www.x.com", "_blank")
+    }
+
+    return(
+        <div className="footer">
+            <div className="left-footer">
+                <img src={logoImage} alt="logo" className="logoImage"/>
+                <p style={{textAlign:"justify",paddingLeft:'2%',margin:'0'}}>
+                Buildmate+ is the ultimate construction enablement platform, offering a seamless experience for clients and professionals alike. With its user-friendly interface, innovative features, and strong community, Buildmate+ is revolutionizing the construction industry, making dreams a reality.
+                </p>
+                <div className="socilMedia">
+                    <img
+                        src={facebook}
+                        alt="facebook"
+                        className="mediaImg"
+                        onClick={openFacebookPage}
+                    />
+                    <img 
+                        src={linkedin} alt="linkedin" className="mediaImg"
+                        onClick={openLinkedin}
+                    />
+                    <img
+                     src={instagram} alt="insta" className="mediaImg"
+                     onClick={openInsta}
+                    />
+                    <img
+                     src={twitter} alt="twitter" className="mediaImg"
+                     onClick={openTwitter}
+                    />
+                </div>
+            </div>
+            <div className="middle-footer">
+                <h1 style={{margin:'0'}}>Support</h1>
+                <p style={{margin:'8% 0 0 0'}}>Help Center</p>
+                <p style={{margin:'8% 0 0 0'}}>Contact Us</p>
+                <p style={{margin:'8% 0 0 0'}}>FQA</p>
+                <p style={{margin:'8% 0 0 0'}}>Community</p>
+            </div>
+            <div className="right-footer">
+                <h1 style={{margin:'0'}}>Contact Us</h1>
+                <p style={{margin:'3% 0 0 0'}}>Feel free to contact & reach us</p>
+                <div className="contact-info">
+                    <div className="contact-logo">
+                        <div className="contact-detail"><img src={phone} alt="phone" className="c-logo"/><p style={{margin:' 0 ',paddingLeft:'5%'}}>123-456-7890</p></div>
+                        <div className="contact-detail"><img src={email} alt="email" className="c-logo"/><p style={{margin:'0',paddingLeft:'5%'}}> info@buildmateplus.com</p></div>
+                        <div className="contact-detail"><img src={gps} alt="gps" className="c-logo"/><p style={{margin:'0 ',paddingLeft:'5%'}}>123 Main Street, City, Country</p></div>
+                    </div>
+                    
+                        
+                        
+                        
+                    
+                </div>
+            </div>
+            
+
+        </div>
+    );
+
+}
+export default footer;
--- Build_Mate_fe/src/Pages/Home.js
+++ Build_Mate_fe/src/Pages/Home.js
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
 import './Home.css';
-
+import CountUp from 'react-countup';
 import homeimg from '../Components/Assets/wall1.jpg';
 import ic1 from '../Components/Assets/ic1.png'; 
 import Navbar from '../Components/Navbar/Navbar';
-import h1 from '../Components/Assets/h1.png';
-import h2 from '../Components/Assets/h2.png';
+import Footer from '../Components/Footer/footer';
 import ic2 from '../Components/Assets/ic2.png';
-import pic1 from '../Components/Assets/pic1.jpg';
+import profile from '../Components/Assets/profile.png';
+import seemore from '../Components/Assets/down-arrow.png'
+import dreamimage from '../Components/Assets/1638943222107.jpeg'
+import oppertunityimg from '../Components/Assets/construction-company-names.jpg'
+import growthImage from '../Components/Assets/growth.png';
+import groupImage from '../Components/Assets/group.png';
+import propertyImage from '../Components/Assets/property.png';
+import starImage from '../Components/Assets/star.png';
+import viewImage from '../Components/Assets/3d-view(1).png';
+
+import ScrollTrigger from "react-scroll-trigger";
 import { Route } from 'react-router-dom';
 
 const Home = () => {
@@ -28,148 +37,166 @@ const Home = () => {
     console.log(`Searching ${searchType} for: ${searchQuery}`);
   };
 
+  const [counterOn, setCounterOn] = useState(false);
+  const pathWindow = () => {
+    // Scroll to the target element
+    const element = document.getElementById('firstbox');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
   return (
    <>
-   <Navbar/>
-    <div className='home'>
+   
+    <div className='homepage'>
+      <Navbar/>
       <img className='wall' src={homeimg} alt='Background' />
       <div className='content'>
         <h1>Connect, Create, Construct</h1>
         <p>Buildmate+ connects countless dreamers with the perfect <br/>
         team to turn their construction visions into reality.</p>
-      </div>
-      <img className='ic1' src={ic1} alt='Icon' />
         
-        <div className="search-bar-container">
-          <div className="switch-bar">
-            <div className="switch-buttons">
-              <button className={`switch-button-ex ${searchType === 'Expert' ? 'active' : ''}`} onClick={() => handleSearchTypeChange('Expert')}>Expert</button>
-              <button className={`switch-button-su ${searchType === 'Supplier' ? 'active' : ''}`} onClick={() => handleSearchTypeChange('Supplier')}>Supplier</button>
-              <button className={`switch-button-ma ${searchType === 'Material' ? 'active' : ''}`} onClick={() => handleSearchTypeChange('Material')}>Material</button>
+      
+        <img className='ic1' src={ic1} alt='Icon' />
+        <div className='search-center'>
+          <div className="search-bar-container">
+            
+              <div className="switch-buttons">
+                <button className={`switch-button-ex ${searchType === 'Expert' ? 'active' : ''}`} onClick={() => handleSearchTypeChange('Expert')}>Expert</button>
+                <button className={`switch-button-su ${searchType === 'Supplier' ? 'active' : ''}`} onClick={() => handleSearchTypeChange('Supplier')}>Supplier</button>
+                <button className={`switch-button-ma ${searchType === 'Material' ? 'active' : ''}`} onClick={() => handleSearchTypeChange('Material')}>Material</button>
+              </div>
+            <div className="search-bar">
+              <input
+                type="text"
+                className="search-input"
+                placeholder={` Search ${searchType.toLowerCase()}`}
+                value={searchQuery}
+                onChange={handleSearchInputChange}
+              />
+              <button className="search-button" onClick={handleSearchSubmit}>Search</button>
             </div>
           </div>
-          <div className="search-bar">
-            <input
-              type="text"
-              className="search-input"
-              placeholder={` Search ${searchType.toLowerCase()}`}
-              value={searchQuery}
-              onChange={handleSearchInputChange}
-            />
-            <button className="search-button" onClick={handleSearchSubmit}>Search</button>
-          </div>
         </div>
+          <div className='arrow'>
+            <img
+             className='downarrow' src={seemore} alt='downarrow'
+             onClick={pathWindow}
+            /></div>
+      </div>
         <div className='path'>
-        <div className='h1'>
-        <img className='h1-img' src={h1} alt='Background' />
-        <div className='h1-content'>
-        <h1 className='h1-text'>Ready to start building your dream?
-            Buildmate+ is here to turn your construction ideas into
-             reality every step of the way.</h1>
-             <button className='h1-button' onClick={HashChangeEvent}>Build your dream </button>
+                      <div className='h1'>
+                       <div className='firstbox'id='firstbox'>
+                          <div className='box1'>
+                            <img src={dreamimage} className='dreamimg'/>
+                            <div className='box'><h1 className='h1-text'>Ready to start building your dream or maintaining your existing construction? 
+                            </h1><button className='h1-button' onClick={HashChangeEvent}>Build your dream </button></div>
+                          </div>
+                          <div className='box2'>
+                            <img src={oppertunityimg} className='dreamimg' style={{ transform: 'scaleX(-1)', zIndex:'-1',borderRadius:'0 65px 65px 0' }}/>
+                            <div className='box'><h1 className='h2-text' style={{transform:'scaleX(-1)'}}>Ready to take on new projects or find exciting job opportunities in the construction industry? 
+                            </h1><button className='h2-button' onClick={HashChangeEvent} style={{transform:'scaleX(-1)', }}>Explore Opportunities</button></div>
+                          </div>
+                      </div> 
+                      <div className='det'>
+                          <div className='details-content'>
+                           
+                            <div className='t1'><ScrollTrigger onEnter={()=> setCounterOn(true)} onExit={()=>setCounterOn(false)} style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+                              <h1 className='h-t1'>{counterOn &&  <CountUp start={0} end={1} duration={2} delay={0}/>}+</h1>
+                              <p className='p-t1'>Years of experience </p></ScrollTrigger> 
+                            </div>
+                            <div className='t1'><ScrollTrigger onEnter={()=> setCounterOn(true)} onExit={()=>setCounterOn(false)} style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+                              <h1 className='h-t1'>{counterOn &&  <CountUp start={0} end={200} duration={2} delay={0}/>}+</h1>
+                              <p className='p-t1'>Number of professionals </p></ScrollTrigger> 
+                            </div>
+                            <div className='t1'><ScrollTrigger onEnter={()=> setCounterOn(true)} onExit={()=>setCounterOn(false)} style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+                              <h1 className='h-t1'>{counterOn &&  <CountUp start={0} end={50} duration={2} delay={0}/>}+</h1>
+                              <p className='p-t1'>Number of projects </p></ScrollTrigger> 
+                            </div>
+                            <div className='t1'><ScrollTrigger onEnter={()=> setCounterOn(true)} onExit={()=>setCounterOn(false)} style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+                              <h1 className='h-t1'>{counterOn &&  <CountUp start={0} end={250} duration={2} delay={0}/>}+</h1>
+                              <p className='p-t1'>Number of suppilers </p></ScrollTrigger> 
+                            </div>
+                          </div>
+                    </div>
+                          <img className='ic2' src={ic2} alt='Icon' />
+                        <div className='wel-all' >
+                          <h1 className='wel-h'>Welcome to BuildMate+</h1>
+                          <p className='wel-p'>Choose BuildMate+ for your construction needs and unlock a world of possibilities. Join our thriving community of builders, designers, and dreamers, and let's build the future together.</p>
+                          </div>
+                      </div>
 
-             </div>
-             <div className='h2'>
-             <img className='h2-img' src={h2} alt='Background'/>
-             <div className='h2-content'>
-              <h1 className='h2-text'>
-              Find trusted professionals for your maintenance needs with Buildmate+. Browse our network of skilled experts and get your project done right.
-              </h1>
-              <button className='h2-button' onClick={HashChangeEvent}>Join your dream</button>
-             </div>
-             </div>
+                          <div className="wel1">
+                            <div className='we-c'>
+                              <img src={propertyImage} alt='property' className='wel-img'/>
+                            <h2 className="we-h">Dream Project Matchmaking</h2>
+                            <p className="we-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
+                            </div>
+                            <div className='we-c'>
+                              <img src={groupImage} alt='group'className='wel-img'/>
+                            <h2 className="we-h">Streamlined Communication</h2>
+                            <p className="we-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero</p>
+                            </div>
+                           
+                              
+                            <div className='we-c'><img src={viewImage} alt='view'className='wel-img'/>
+                            <h2 className="we-h">Seamless 3D Design Integration</h2>
+                            <p className="we-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero</p>
+                            </div>
 
-            <div className='det'>
-             <div className='details-content'>
-              <div className='t1'>
-                <h1 className='h-t1'>01+</h1>
-                <p className='p-t1'>Years of experience </p>
-              </div>
-              <div className='t2'>
-                <h1 className='h-t2'>100+</h1>
-                <p className='p-t2'>Number of professionals </p>
-              </div>
-              <div className='t3'>
-                <h1 className='h-t3'>50+</h1>
-                <p className='p-t3'>Number of projects </p>
-              </div>
-              <div className='t4'>
-                <h1 className='h-t4'>250+</h1>
-                <p className='p-t4'>Number of suppilers </p>
-              </div>
-             </div>
-             </div>
-             <img className='ic2' src={ic2} alt='Icon' />
-             <h1 className='wel-h'>Welcome to BuildMate+</h1>
-             <p className='wel-p'>Choose BuildMate+ for your construction needs and unlock a world of possibilities. Join our thriving community of builders, designers, and dreamers, and let's build the future together.</p>
-             </div>
+                            <div className='we-c'>
+                              <img src={growthImage} alt='growth'className='wel-img'/>
+                            <h2 className="we-h">Transparent Progress Tracking</h2>
+                            <p className="we-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero</p>
+                            </div>
+                          </div>
 
-             <div className="wel1">
-             <div className='we-c'>
-             <h2 className="we-h">Dream Project Matchmaking</h2>
-             <p className="we-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
-             </div>
-             <div className="wel1">
-             <div className='we-c'>
-             <h2 className="we-h">Streamlined Communication</h2>
-             <p className="we-p">Seamlessly communicate and collaborate with your team or clients, ensuring everyone is on the same page from start to finish.</p>
-             </div>
-             </div>
+                          <div className='re-con'>
+                            <h1 className='re-h'>We care about our customer<br/> experience too </h1>
+                            <div className='review-con'>
 
-             <div className="wel1">
-             <div className='we-c'>
-             <h2 className="we-h">Seamless 3D Design Integration</h2>
-             <p className="we-p">Visualize your dream construction projects with our advanced 3D modeling tools, ensuring your vision comes to life exactly as you imagine.</p>
-             </div>
-             </div>
+                            <div className="con-1">
+                              <img src={profile} alt="Image" className='profile-img' style={{width:'70px'}} />
+                              <p className='re'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis donec et odio pellentesque diam volutpat commodo sed.</p>
+                              <div className='line-re'></div>
+                              <div className='footer-con'>
+                                <div className='position'>
+                              <p className='re-name'>John Walker</p>
+                              <p className='re-po'>Architecture</p></div>
+                              <div className='review-star'>
+                                <img src={starImage} alt='star' className='star' style={{width:'15px', filter: 'grayscale(100%)'}}/>
+                                <img src={starImage} alt='star' className='star' style={{width:'15px'}}/>
+                                <img src={starImage} alt='star' className='star' style={{width:'15px'}}/>
+                                <img src={starImage} alt='star' className='star' style={{width:'15px'}}/>
+                                <img src={starImage} alt='star' className='star' style={{width:'15px'}}/>
+                              </div>
+                              </div>
+                            </div>
+                            
 
-             <div className="wel1">
-             <div className='we-c'>
-             <h2 className="we-h">Transparent Progress Tracking</h2>
-             <p className="we-p">Stay updated on your project's progress with real-time tracking and reporting features, giving you peace of mind and control.</p>
-             </div>
-             </div>
+                            <div className='con-1'>
+                            <img src={profile} alt="Image" style={{width:'70px'}} className='profile-img'/>
+                            <p className='re'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis donec et odio pellentesque diam volutpat commodo sed.</p>
+                            <div className='line-re'></div>
+                            <p className='re-name'>John Walker</p>
+                            <p className='re-po'>Architecture</p>
+                            </div>
 
-             <div className='re-con'>
-             <h1 className='re-h'>We care about our customer experience too </h1>
-             <div className="con-1">
-             <div className="round-con-1">
-             <img src={pic1} alt="Image" />
-             </div>
-                <p className='re'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis donec et odio pellentesque diam volutpat commodo sed.</p>
-                <p className='line-re'>______________________________________________</p>
-                <p className='re-name'>John Walker</p>
-                <p className='re-po'>Architecture</p>
-              </div>
-              
+                            <div className='con-1'>
+                            <img src={profile} alt="Image" style={{width:'70px'}} className='profile-img'/>
+                            <p className='re'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis donec et odio pellentesque diam volutpat commodo sed.</p>
+                            <div className='line-re'></div>
+                            <p className='re-name'>John Walker</p>
+                            <p className='re-po'>Architecture</p>
+                            </div>
 
-              <div className='con-2'>
-              <div className="round-con-2">
-              <img src={pic1} alt="Image" />
-              </div>
-              <p className='re'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis donec et odio pellentesque diam volutpat commodo sed.</p>
-              <p className='line-re'>______________________________________________</p>
-              <p className='re-name'>John Walker</p>
-              <p className='re-po'>Architecture</p>
-              </div>
+                          </div>
+                          </div>    
+                          <Footer/>     
 
-              <div className='con-3'>
-              <div className="round-con-3">
-              <img src={pic1} alt="Image" />
-              </div>
-              <p className='re'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis donec et odio pellentesque diam volutpat commodo sed.</p>
-              <p className='line-re'>______________________________________________</p>
-              <p className='re-name'>John Walker</p>
-              <p className='re-po'>Architecture</p>
-              </div>
-
-            
-             </div>    
-                    
-
-    </div>
                   
-        
+                                
+                      
       </div>
     </div>
    </>
